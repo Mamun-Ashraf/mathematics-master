d2a76b5ef8d4b17aeffa791e719ace5e314313f5
Mamun-Ashrsf <ashrafdkia@gmail.com>
2023-11-07 15:40:28 +0600

    setup basic project

---
+++ JS/app.js
@@ -0,0 +1,21 @@
+function getRandomColor() {
+    const red = Math.floor(Math.random() * 256);
+    const green = Math.floor(Math.random() * 256);
+    const blue = Math.floor(Math.random() * 256);
+
+    return `rgb(${red},${green},${blue})`;
+}
+
+const hoverColorCards = document.querySelectorAll('.hover-random-bg');
+
+for (const hoverColorCard of hoverColorCards) {
+    hoverColorCard.addEventListener('mouseenter', function () {
+        hoverColorCard.style.backgroundColor = getRandomColor();
+    });
+}
+
+for (const hoverColorCard of hoverColorCards) {
+    hoverColorCard.addEventListener('mouseleave', function () {
+        hoverColorCard.style.backgroundColor = '#e2e8f0';
+    })
+}
